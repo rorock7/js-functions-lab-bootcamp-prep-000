476ac70cf7e8a3b8dcef6dc2cf6c0311af5e8389
Rohit Gangurde <gangurde077@gmail.com>
2017-12-30 00:51:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ return ('Happy holidays!')
 }
 function happyHolidaysTo(name)
 {
-  return (`Happy holidays, ${name}!`)
+  return(`Happy holidays, ${name}`)
 }
-function happyHolidaysTo(holiday,name)
-{
-  return (`Happy ${holiday}, ${name}!`)
-}
-function holidayCountdown(days,holiday)
-{
-  return (`It's ${days} days unitl ${holiday}!`)
-}
-happyHolidays()
 happyHolidaysTo('Rohit')
-happyHolidaysTo("Christmas","Rohit")
+
+happyHolidays()
+function happyHolidayTo(holiday, name)
+{
+  return(`Happy ${holiday}, ${name}!`)
+}
+happyHolidayTo("Christmas","Rohit")
+function holidayCountdown(days, holiday)
+{
+  return(`It's ${days} days until ${holiday}!`)
+}
 holidayCountdown("12","Christmas")
