6876932eb0853603469d28b12c2d71b1683ff70f
Rohit Gangurde <gangurde077@gmail.com>
2017-12-30 00:41:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+
+function happyHolidays()
+{
+return ('Happy holidays!')
+}
+function happyHolidaysTo(name)
+{
+  return (`Happy holidays, ${name}!`)
+}
+function happyHolidaysTo(holiday,name)
+{
+  return (`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(days,holiday)
+{
+  return (`It's ${days} days unitl ${holiday}!`)
+}
+happyHolidays()
+happyHolidaysTo('Rohit')
+happyHolidaysTo("Christmas","Rohit")
+holidayCountdown("12","Christmas")
