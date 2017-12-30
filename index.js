5338106f67357e32adae7ea27cc2334d3d68f1e2
Rohit Gangurde <gangurde077@gmail.com>
2017-12-30 00:55:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function happyHolidayTo(holiday, name)
   return(`Happy ${holiday}, ${name}!`)
 }
 happyHolidayTo('Happy Independence Day','you')
-function holidayCountdown(days, holiday)
+function holidayCountdown(holiday,days)
 {
   return(`It's ${days} days until ${holiday}!`)
 }
-holidayCountdown("20","Mother's Day")
+holidayCountdown("Mother's Day","20")
